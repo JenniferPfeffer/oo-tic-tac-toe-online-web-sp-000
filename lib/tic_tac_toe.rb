7b8175fc52a0204ce86174fb3a9c5e07236d3289
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-17 15:43:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,12 +18,12 @@ def move( index, current_player = "X")
   @board[index] = current_player
 end
 
-def position_taken?(@board, location)
+def position_taken?(board, location)
   @board[location] != " " && @board[location] != ""
 end
 
-def valid_move?(@board, index)
-  index.between?(0,8) && !position_taken?(@board, index)
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
