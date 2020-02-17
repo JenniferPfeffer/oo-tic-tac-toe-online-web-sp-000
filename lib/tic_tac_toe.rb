c1e6d500236bebafa531e82d088eb4e2be871a7c
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-17 16:02:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,7 +108,17 @@ def winner
   end
 end
 def play 
-  
+  until over? == true
+    turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
   
 
     
